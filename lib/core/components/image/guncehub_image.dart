365b5fa7118299c2e218458a93478a08abcfc11c
HamitTirpanGunce <hamit.tirpan@guncehub.com>
2022-05-20 01:09:35 +0300

    add native splash screens

--- lib/core/components/image/guncehub_image.dart
+++ lib/core/components/image/guncehub_image.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 class GunceHubImage extends StatelessWidget {
-  const GunceHubImage({Key? key, required this.assetName}) : super(key: key);
+  const GunceHubImage(
+      {Key? key, required this.assetName, this.width, this.height})
+      : super(key: key);
   final String assetName;
-
+  final int? width;
+  final int? height;
   @override
   Widget build(BuildContext context) {
     return Image.asset(
       assetName,
-      cacheHeight: 220,
-      cacheWidth: 150,
+      cacheWidth: width ?? 61,
+      cacheHeight: height ?? 90,
     );
   }
 }
